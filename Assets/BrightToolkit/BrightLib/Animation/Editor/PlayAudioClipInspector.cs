7306d008a73a04a809881a6d81699e4abb5e3984
kitipop <kittipop_ch@cmu.ac.th>
2022-11-05 22:52:23 +0700

    test
    tataatatatata

---
+++ Assets/BrightToolkit/BrightLib/Animation/Editor/PlayAudioClipInspector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace BrightLib.Animation
+{
+    [CustomEditor(typeof(PlayAudioClip))]
+    public class PlayAudioClipInspector : Editor 
+    {
+        public override void OnInspectorGUI () 
+        {
+            // DrawDefaultInspector();
+            var tObject = (target as PlayAudioClip);
+
+            tObject.useMultiple = EditorGUILayout.Toggle("Use Multiple", tObject.useMultiple);
+            if(!tObject.useMultiple)
+            {
+                tObject.clip = (AudioClip)EditorGUILayout.ObjectField("Clip", tObject.clip, typeof(AudioClip));
+            }
+            else 
+            {
+                var prop = serializedObject.FindProperty("clips");
+                EditorGUILayout.PropertyField(prop, true);
+            }
+
+            tObject.delay = EditorGUILayout.FloatField("Delay", tObject.delay);
+            tObject.condition = (PlayCondition)EditorGUILayout.EnumPopup("Condition", tObject.condition);
+            if(tObject.condition == PlayCondition.OnUpdate)
+            {
+                EditorGUI.indentLevel++;
+                tObject.onUpdateInterval = EditorGUILayout.FloatField("Frequency", tObject.onUpdateInterval);
+                EditorGUI.indentLevel--;
+            }
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/BrightToolkit/BrightLib/Animation/PlayAudioClip.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace BrightLib.Animation
+{
+	public enum PlayCondition{OnEnter, OnUpdate, OnExit}
+
+	public class PlayAudioClip : StateMachineBehaviour 
+	{
+		public bool useMultiple;
+		public AudioClip clip;
+		public AudioClip[] clips;
+		private int _clipIndex;
+		public float delay;
+		public PlayCondition condition;
+		[Tooltip("What's the interval between playing the audio on the update")]
+		public float onUpdateInterval;
+		private float _lastUpdateTime;
+
+		private AudioSource _source;
+
+		// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+		override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if(_source == null) FetchAudioSource(animator);
+			_lastUpdateTime = Time.time;
+
+			if(condition != PlayCondition.OnEnter) return;
+			Execute();
+		}
+
+		// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+		override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if(condition != PlayCondition.OnUpdate) return;
+
+			if(Time.time - _lastUpdateTime < onUpdateInterval) return;
+			_lastUpdateTime = Time.time;
+			Execute();
+		}
+
+		// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+		override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if(condition != PlayCondition.OnExit) return;
+			Execute();
+		}
+
+		private void Execute()
+		{
+			if(_source == null) throw new NullReferenceException();
+			if(useMultiple && clips == null) throw new NullReferenceException();
+			if(useMultiple && clips.Length == 0) throw new IndexOutOfRangeException();
+
+			_source.clip = !useMultiple ? clip : clips[_clipIndex++ % clips.Length];
+			_source.PlayDelayed(delay);
+		}
+
+
+		private void FetchAudioSource(Animator animator)
+		{
+			_source = animator.GetComponent<AudioSource>();
+			if(_source == null)
+			{
+				Debug.LogWarning("PlayAudioClip: No AudioSource found on the Animator's GameObject");
+			}
+		}
+	}
+
+}
